89f4db6a0bec4e2f5e2894196209ee8212a1508e
Andrew Stanton <refefer@gmail.com>
2014-12-23 08:20:21 -0800

    More work with generics

--- function.py
+++ function.py
@@ -1,30 +1,47 @@
 from functools import partial
 from itertools import izip
 
-from generics import GenericType
+from generics import GenericType, is_generic
 
 class Function(object):
 
-    def __init__(self, types, ret, f):
+    def __init__(self, types, ret, f, is_method=False):
+        assert isinstance(types, tuple)
         assert all(isinstance(t, type) for t in types)
         assert isinstance(ret, type)
-        assert f.func_code.co_argcount == len(types)
+        if __debug__:
+            argsN = f.func_code.co_argcount
+            print len(types), argsN
+            # Ignore self/cls
+            if is_method:
+                assert  argsN == (len(types) + 1)
+            else:
+                assert argsN == len(types)
+
+        self.method = is_method
         self.types = types 
-        self.generic = any(issubclass(t, GenericType) for t in types)
-        self.cardinality = len(types)
+        self.generics = list({t for ts in (types, (ret,)) 
+            for t in ts if is_generic(t)})
+
+        self.polymorphic = len(self.generics) > 0
+        self.cardinality = len(types) + 1 if is_method else len(types)
         self.ret = ret
         self.f = f
 
     def __call__(self, *args):
+
+        # Handle methods
+        types = self.types if not self.method else (object,) + self.types 
         if __debug__:
+            print len(args), self.cardinality
             assert len(args) == self.cardinality
 
         # Generic?  Gotta build a fully formed function
-        if self.generic:
-            return self._callGeneric(args)
+        if self.polymorphic:
+            return self._callGeneric(types, args)
         
         if __debug__:
-            for p, t in izip(args, self.types):
+            for p, t in izip(args, types):
                 assert isinstance(p, t), \
                     "%s: Expected %s, got %s" % (
                         self.f.__name__, t.__name__, type(p).__name__
@@ -37,32 +54,37 @@ class Function(object):
                         self.f.__name__, self.ret.__name__, type(ret).__name__
                 )
                                              
-
         return ret
 
-    def _callGeneric(self, args):
-        gens = {}
-        types = []
-        for t, v in izip(self.types, args):
-            if issubclass(t, GenericType):
-                if t not in gens:
-                    gens[t] = type(v)
+    def _callGeneric(self, types, args):
+        gs = [type(t) for T, t in izip(types, args) if is_generic(T)]
+        new_f = self[tuple(gs)]
+        # Return type _must_ be known at call time
+        assert not is_generic(new_f.ret)
+        return new_f(*args)
 
-                types.append(gens[t])
+    def __getitem__(self, ts):
+        """
+        Take generics, make them less generic!
+        """
+        ts = ts if isinstance(ts, tuple) else (ts,)
+        assert len(self.generics) == len(ts)
 
-        # Resolve ret
-        ret = gens[self.ret] if issubclass(self.ret, GenericType) else self.ret
-            
-        return Function(types, ret, self.f)(*args)
+        gen_map = {g: t for g, t in izip(self.generics, ts)}
+        new_types = [gen_map.get(t, t) for t in self.types]
+        
+        # Resolve return type, if we can
+        ret = gen_map[self.ret] if is_generic(self.ret) else self.ret
+
+        return Function(tuple(new_types), ret, self.f, is_method=self.method)
 
     def andThen(self, func):
         assert isinstance(func, Function)
         assert func.cardinality == 1
         assert issubclass(self.ret, func.types[0])
         
-        this = self
         def _f(*args, **kwargs):
-            return func(*this(*args, **kwargs))
+            return func(*self(*args, **kwargs))
 
         return Function(self.types, func.ret, _f)
 
@@ -88,7 +110,13 @@ class Function(object):
         return self
 
     def __str__(self):
-        return "%s(%s): %s" % (self.f.__name__, 
+        if self.polymorphic:
+            gtypes = '[%s]' % ", ".join(t.__name__ for t in self.generics)
+        else:
+            gtypes = ''
+
+        return "%s%s(%s): %s" % (self.f.__name__, 
+                gtypes,
                 ", ".join(t.__name__ for t in self.types),
                 self.ret.__name__)
 
@@ -97,9 +125,19 @@ class Function(object):
 def F0(ret):
     return partial(Function, (), ret)
 
+def F0M(ret):
+    return partial(Function, (), ret, is_method=True)
+
 def FUnit(*types):
     return partial(Function, types, NoneType)
+
+def FUnitM(*types):
+    return partial(Function, types, NoneType, is_method=True)
 
 def Func(*types):
     assert len(types) > 0
     return partial(Function, tuple(types[:-1]), types[-1])
+
+def Method(*types):
+    assert len(types) > 0
+    return partial(Function, tuple(types[:-1]), types[-1], is_method=True)
--- generics.py
+++ generics.py
@@ -1,3 +1,4 @@
+from itertools import izip
 
 class GenericType(type): pass
     
@@ -5,3 +6,6 @@ A = type('A', (GenericType,), {})
 B = type('B', (GenericType,), {})
 C = type('C', (GenericType,), {})
 D = type('D', (GenericType,), {})
+
+is_generic = lambda t: issubclass(t, GenericType)
+
